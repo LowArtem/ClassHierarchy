e7709a4142c2c9ae59a5781c94a23740b31fbe37
Artem Zhikharev <artemziharew@gmail.com>
2022-06-14 14:51:03 +0300

    Добавлены пустые заглушки для всех функций

--- main.cpp
+++ main.cpp
@@ -10,3 +10,54 @@ int main(int argc, char *argv[])
 
     return 0;
 }
+
+void parseInputFile(QString inputFile, QList<ClassInfo> &classes)
+{
+
+}
+
+void parseString(QString string, struct ClassInfo &classs)
+{
+
+}
+
+void getHighestClasses(QList<struct ClassInfo> &classes, QList<struct ClassInfo> &highestClasses)
+{
+
+}
+
+void getClassesWithGivenProperties(QList<QString> propertyNames, QList<struct ClassInfo> &classes, QList<struct ClassInfo> &classesWithGivenProperty)
+{
+
+}
+
+bool IsClassPropertiesEqual(struct ClassInfo &class1, struct ClassInfo &class2, QString propertyName)
+{
+    return false;
+}
+
+void splitByHierarchy(QList<struct ClassInfo> &classes)
+{
+
+}
+
+void splitByRelationship(QList<struct ClassInfo> &classes)
+{
+
+}
+
+void writeToXML(QList<struct ClassInfo> &classes, QString outputFile)
+{
+
+}
+
+void writeClassToXML(struct ClassInfo &classs, QList<QString> &outputXML, QList<int> &closedTagsStringIndexes)
+{
+
+}
+
+
+
+
+
+
